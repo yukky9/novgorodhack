4be307153abcbfcac24b163883be6fc414e541da
yukky9 <kozhurinayulia@gmail.com>
2024-06-01 18:42:05 +0300

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'yellow': 'var(--yellow)',
+        'light-yellow': 'var(--light-yellow)',
+        'light-body': 'var(--light-body)',
+        'light-grey': 'var(--light-grey)',
+        'blue': 'var(--blue)',
+        'dark-blue': 'var(--dark-blue)',
+        'dark-gold': 'var(--dark-gold)',
+        'grey': 'var(--grey)'
+      }
+    },
   },
   plugins: [],
 }
