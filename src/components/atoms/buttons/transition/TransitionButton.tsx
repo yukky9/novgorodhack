4c8b2eb9b8d9898029a622325ef0096cb5ae9e86
yukky9 <kozhurinayulia@gmail.com>
2024-06-01 18:42:38 +0300

    transition button

---
+++ src/components/atoms/buttons/transition/TransitionButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const TransitionButton = () => {
+    return (
+        <button
+            className="w-64 bg-transparent hover:bg-yellow text-dark-blue font-semibold hover:text-black py-2 px-4 border-2 border-dark-blue hover:border-transparent rounded-xl">
+            Перейти
+        </button>
+    );
+};
+
+export default TransitionButton;
