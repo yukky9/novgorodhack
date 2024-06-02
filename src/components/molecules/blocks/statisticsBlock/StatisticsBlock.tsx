56c9d59e735ac031e3df346b213b76cccbbf9231
yukky9 <kozhurinayulia@gmail.com>
2024-06-02 17:01:06 +0300

    statistics

--- src/components/molecules/blocks/statisticsBlock/StatisticsBlock.tsx
+++ src/components/molecules/blocks/statisticsBlock/StatisticsBlock.tsx
@@ -5,10 +5,12 @@ import GeneralText from '../../../atoms/text/generalText/GeneralText';
 
 const StatisticsBlock = () => {
     return (
-        <div className='bg-white shadow-2xl border-2 border-yellow max-w-sm min-h-96 py-10 px-4 text-center rounded-2xl'>
-            <img className='rounded-2xl w-72 mx-auto mb-2' src={logo} alt='statistic'/>
+        <div className='bg-white shadow-2xl border-2 border-yellow max-w-sm min-h-96 py-12 px-4 text-center rounded-2xl'>
+            <img className='rounded-2xl h-auto w-80 mx-auto mb-5' src={logo} alt='statistic'/>
             <GeneralText title='Статистика'/>
-            <a href='/statistics'><TransitionButton text='Перейти'/></a>
+            <div className='-mb-5'>
+                <a href='/statistics'><TransitionButton text='Перейти'/></a>
+            </div>
         </div>
     );
 };
