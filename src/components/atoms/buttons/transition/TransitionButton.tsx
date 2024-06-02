313f6b4589b6aed78282228e9963ea13b6100230
yukky9 <kozhurinayulia@gmail.com>
2024-06-02 16:33:57 +0300

    button

--- src/components/atoms/buttons/transition/TransitionButton.tsx
+++ src/components/atoms/buttons/transition/TransitionButton.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 type Props = {
-    text: string,
-    onClick?: any
+    text: string
 }
 
-const TransitionButton = ({text, onClick}: Props) => {
+const TransitionButton = ({text}: Props) => {
     return (
         <button
             className="w-64 mb-3 bg-transparent hover:bg-yellow text-dark-blue font-semibold hover:text-black py-2 px-4 border-2 border-dark-blue hover:border-transparent rounded-xl">
