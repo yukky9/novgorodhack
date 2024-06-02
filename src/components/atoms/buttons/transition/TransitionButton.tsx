bd78a06af659d359309e0a8f9179415fff22ea9a
yukky9 <kozhurinayulia@gmail.com>
2024-06-02 17:05:09 +0300

    button

--- src/components/atoms/buttons/transition/TransitionButton.tsx
+++ src/components/atoms/buttons/transition/TransitionButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 const TransitionButton = ({text}: Props) => {
     return (
         <button
-            className="w-64 mb-3 bg-transparent hover:bg-yellow text-dark-blue font-semibold hover:text-black py-2 px-4 border-2 border-dark-blue hover:border-transparent rounded-xl">
+            className="w-64 mb-3 hover:bg-yellow text-dark-blue font-semibold hover:text-black bg-grey py-2 px-4 border-2 border-dark-blue hover:border-transparent rounded-xl">
             {text}
         </button>
     );
