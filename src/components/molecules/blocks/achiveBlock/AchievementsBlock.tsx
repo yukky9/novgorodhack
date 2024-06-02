492778503e2fa40319f6d138de230de9783d8bc1
yukky9 <kozhurinayulia@gmail.com>
2024-06-02 14:54:19 +0300

    achievements block

---
+++ src/components/molecules/blocks/achiveBlock/AchievementsBlock.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import TransitionButton from '../../../atoms/buttons/transition/TransitionButton';
+import logo from '../../../atoms/image/general/achieve.png';
+import GeneralText from '../../../atoms/text/generalText/GeneralText';
+
+const AchievementsBlock = () => {
+    return (
+        <div className='bg-white shadow-2xl border-2 border-blue max-w-sm py-2 px-4 text-center rounded-2xl'>
+            <img className='rounded-2xl w-72 mx-auto' src={logo} alt='achieve'/>
+            <GeneralText title='Достижения'/>
+            <TransitionButton text='Перейти'/>
+        </div>
+    );
+};
+
+export default AchievementsBlock;
