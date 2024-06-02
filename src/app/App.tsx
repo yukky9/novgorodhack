0690ea68e342585db1bd7b84762088a07c17e1be
yukky9 <kozhurinayulia@gmail.com>
2024-06-02 16:31:47 +0300

    router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../app/index.css';
+import MainRouter from '../router/MainRouter/MainRouter';
+
+
 
 function App() {
   return (
       <div>
+          <MainRouter/>
       </div>
   );
 }
--- src/router/MainRouter/MainRouter.tsx
+++ src/router/MainRouter/MainRouter.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AchivementsPage from '../../components/page/AchivementsPage/AchivementsPage';
+import GeneralPage from '../../components/page/GeneralPage/GeneralPage';
+import StatisticsPage from '../../components/page/StatisticsPage/StatisticsPage';
+import TasksPage from '../../components/page/TasksPage/TasksPage';
 
 const MainRouter = () => {
     return (
-        <div>
-            
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<GeneralPage/>}/>
+                <Route path="/statistics" element={<StatisticsPage/>}/>
+                <Route path="/tasks" element={<TasksPage/>}/>
+                <Route path="/achievements" element={<AchivementsPage/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
