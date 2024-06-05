cd28b9ae9f4efe5f9182d4328036f7ce3ed6f7a1
yukky9 <kozhurinayulia@gmail.com>
2024-06-05 18:39:31 +0300

    correct

--- src/components/molecules/blocks/achiveBlock/AchievementsBlock.tsx
+++ src/components/molecules/blocks/achiveBlock/AchievementsBlock.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import TransitionButton from '../../../atoms/buttons/transition/TransitionButton';
 import logo from '../../../atoms/image/general/achieve.png';
-import GeneralText from '../../../atoms/text/generalText/GeneralText';
+import CardText from '../../../atoms/text/generalText/CardText';
 
 const AchievementsBlock = () => {
     return (
         <div className='bg-white shadow-2xl border-2 border-blue max-w-sm py-2 px-4 text-center rounded-2xl'>
             <img className='rounded-2xl w-72 mx-auto' src={logo} alt='achieve'/>
-            <GeneralText title='Достижения'/>
+            <CardText title='Достижения'/>
             <a href='/achievements'><TransitionButton text='Перейти'/></a>
         </div>
     );
--- src/components/molecules/blocks/statisticsBlock/StatisticsBlock.tsx
+++ src/components/molecules/blocks/statisticsBlock/StatisticsBlock.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import TransitionButton from '../../../atoms/buttons/transition/TransitionButton';
 import logo from '../../../atoms/image/general/statistics.png';
-import GeneralText from '../../../atoms/text/generalText/GeneralText';
+import CardText from '../../../atoms/text/generalText/CardText';
 
 const StatisticsBlock = () => {
     return (
         <div className='bg-white shadow-2xl border-2 border-yellow max-w-sm min-h-96 py-12 px-4 text-center rounded-2xl'>
             <img className='rounded-2xl h-auto w-80 mx-auto mb-5' src={logo} alt='statistic'/>
-            <GeneralText title='Статистика'/>
+            <CardText title='Статистика'/>
             <div className='-mb-5'>
                 <a href='/statistics'><TransitionButton text='Перейти'/></a>
             </div>
